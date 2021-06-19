9c72d7ac01b6592ab2fecc6199b4a409a42b05ba
Jagankumar-Egov <jagan.kumar@egovernments.org>
2021-06-19 06:37:23 +0530

    just add that

--- src/app.js
+++ src/app.js
@@ -1,15 +1,34 @@
+import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
 import React, { Component } from "react";
-import Calculator from "./components/calculator";
-
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import Home from "./screens/home";
+import themeObject from './theme';
+import Calculator from "./components/calculator"
 
 class App extends Component {
+  
     render() {
+    
         return (
             <div>
-                <Calculator></Calculator>
+                                        <Calculator></Calculator>
+                
             </div>
         )
     }
 }
 
 export default App;
+
+
+
+
+
+VUL-WEB-H003	Insecure Direct Object References (IDOR)
+VUL-WEB-L002	Sensitive Information in URL
+VUL-WEB-L006	Clickjacking
+SCR-DIGIT-M007	It was observed that the application uses eval(code).
+SCR-DIGIT-L010	Do not use dangerouslySetInnerHTML property in React components.
+SCR-DIGIT-L009	Do not release debuggable apps
+SCR-DIGIT-L004	Avoid post cross-document messages with an overly permissive target origin
